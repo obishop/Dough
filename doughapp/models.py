87a23328b5ebc5c539486bc4c0be315977646747
Olivia Bishop <obishop89@gmail.com>
2011-04-27 00:18:19 -0400

    added user to models

--- doughapp/models.py
+++ doughapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
 class FoodItem(models.Model):
@@ -7,7 +8,9 @@ class FoodItem(models.Model):
     location = models.IntegerField()
     category = models.CharField(max_length=64)
     purchase = models.ForeignKey('Purchase')
+    user = models.ForeignKey(User)
 
 class Purchase(models.Model):
     amount = models.FloatField()
     date = models.DateField()
+    user = models.ForeignKey(User)
