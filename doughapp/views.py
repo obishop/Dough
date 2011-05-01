7ef63a2fdc2efd2f940552600a56717f88de306f
kvontish <kvontish@gmail.com>
2011-05-01 17:30:27 -0400

    changing recipe formatting

--- doughapp/views.py
+++ doughapp/views.py
@@ -94,7 +94,7 @@ def budget(request):
 @login_required
 def recipes(request):
 	food_list = FoodItem.objects.filter(user=request.user)
-    return render_to_response('recipes.html', {'food_list': food_list})
+	return render_to_response('recipes.html', {'food_list': food_list})
 
 
    
